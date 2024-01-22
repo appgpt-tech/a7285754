6d4c9d7f8699cf1e28202c1480f4c181905f717b
11ai <124818466+11ai@users.noreply.github.com>
2024-01-22 22:23:15 +0200

    edited on Mon Jan 22 2024 20:23:08 GMT+0000 (Coordinated Universal Time)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -75,7 +75,7 @@ const App = () => (
 list={BooksList}
 create={BooksCreate}
 edit={BooksEdit}
-recordRepresentation="booktitle"
+recordRepresentation="title"
 icon={BooksIcon}/>
     <CustomRoutes noLayout>
       {/*This renders the login UI on the /auth route*/}
