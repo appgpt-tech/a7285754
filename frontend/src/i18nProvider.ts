f7a86d1903b5e707d0a4058b181d055123ea25f2
11ai <124818466+11ai@users.noreply.github.com>
2024-01-22 21:27:01 +0200

    edited on Mon Jan 22 2024 19:26:47 GMT+0000 (Coordinated Universal Time)

--- frontend/src/i18nProvider.ts
+++ frontend/src/i18nProvider.ts
@@ -3,7 +3,7 @@
     import polyglotI18nProvider from "ra-i18n-polyglot";
     import enOriginal from 'ra-language-english';
 
-    const enResources = { resources: {"Books":{"name":"books","fields":{"Booktitle":"Booktitle","Releasedate":"Releasedate","Authorname":"Authorname","Id":"Id"}}}};
+    const enResources = { resources: {"Books":{"name":"Books","fields":{"Booktitle":"Booktitle","Releasedate":"Releasedate","Authorname":"Authorname","Authorgender":"Authorgender","Id":"Id"}}}};
 
 
     const en = mergeTranslations(enOriginal,enResources);
