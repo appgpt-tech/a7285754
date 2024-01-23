461758f8d5055e9240cca7fefbad73c0d1fd2fe4
11ai <124818466+11ai@users.noreply.github.com>
2024-01-23 21:48:25 +0200

    edited on Tue Jan 23 2024 19:48:17 GMT+0000 (Coordinated Universal Time)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,9 @@ import { i18nProvider } from "./i18nProvider";
 import LoginPage, { Login } from "./Login";
 import data from "./data";
 import { BooksList, BooksCreate, BooksEdit} from "./resources/Books";
-import BooksIcon from "@mui/icons-material/Book"; 
+import { AuthorsList, AuthorsCreate, AuthorsEdit} from "./resources/Authors";
+import BooksIcon from "@mui/icons-material/Book";
+import AuthorsIcon from "@mui/icons-material/Person"; 
 // SUPERTOKENS
 import React from "react";
 import SuperTokens, {
@@ -77,6 +79,12 @@ create={BooksCreate}
 edit={BooksEdit}
 recordRepresentation="booktitle"
 icon={BooksIcon}/>
+<Resource name="Authors" options={{label:"Authors"}} 
+list={AuthorsList}
+create={AuthorsCreate}
+edit={AuthorsEdit}
+recordRepresentation="authorname"
+icon={AuthorsIcon}/>
     <CustomRoutes noLayout>
       {/*This renders the login UI on the /auth route*/}
       {getSuperTokensRoutesForReactRouterDom(reactRouterDom)}
