36c0c406e4b77caf4c4c536df767861fdedd5c11
11ai <124818466+11ai@users.noreply.github.com>
2024-01-27 00:08:46 +0200

    edited on Sat Jan 27 2024 00:08:31 GMT+0200 (Eastern European Standard Time)

--- frontend/src/data.ts
+++ frontend/src/data.ts
@@ -1,3 +1,3 @@
 
-let data: any = {"defaultData":{}};
+let data: any = {"defaultData":{"Books":[{"title":"Book Title 1","author":"Author 1","isbn":"ISBN-111","copiesSold":100,"currentEdition":"1st Edition","issueDate":"2020-01-01"},{"title":"Book Title 2","author":"Author 2","isbn":"ISBN-222","copiesSold":200,"currentEdition":"2nd Edition","issueDate":"2021-01-01"},{"title":"Book Title 3","author":"Author 3","isbn":"ISBN-333","copiesSold":300,"currentEdition":"3rd Edition","issueDate":"2022-01-01"}]}};
 export default data;
