fe77e1cbf85277161e080f5acd2727ae7fd57539
11ai <124818466+11ai@users.noreply.github.com>
2024-01-27 00:08:54 +0200

    edited on Sat Jan 27 2024 00:08:31 GMT+0200 (Eastern European Standard Time)

--- backend/src/Database.ts
+++ backend/src/Database.ts
@@ -24,7 +24,7 @@ export class Database {
     await Database.Seed();
   }
   static async Seed() {
-    let data: any = {};
+    let data: any = {"Books":[{"title":"Book Title 1","author":"Author 1","isbn":"ISBN-111","copiesSold":100,"currentEdition":"1st Edition","issueDate":"2020-01-01"},{"title":"Book Title 2","author":"Author 2","isbn":"ISBN-222","copiesSold":200,"currentEdition":"2nd Edition","issueDate":"2021-01-01"},{"title":"Book Title 3","author":"Author 3","isbn":"ISBN-333","copiesSold":300,"currentEdition":"3rd Edition","issueDate":"2022-01-01"}]};
     //Autogenerate multiple such calls ie for each resource and its data object
     let isSeeded = await this.IsSeeded();
     //if (!isSeeded) {
