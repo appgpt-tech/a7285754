050773b4add82d6fbd44fcdd3bcd50418e620404
11ai <124818466+11ai@users.noreply.github.com>
2024-01-27 00:18:56 +0200

    edited on Sat Jan 27 2024 00:18:32 GMT+0200 (Eastern European Standard Time)

--- backend/src/Database.ts
+++ backend/src/Database.ts
@@ -24,7 +24,7 @@ export class Database {
     await Database.Seed();
   }
   static async Seed() {
-    let data: any = {"Books":[{"title":"Book Title 1","author":"Author 1","isbn":"ISBN-111","copiesSold":100,"currentEdition":"1st Edition","issueDate":"2020-01-01"},{"title":"Book Title 2","author":"Author 2","isbn":"ISBN-222","copiesSold":200,"currentEdition":"2nd Edition","issueDate":"2021-01-01"},{"title":"Book Title 3","author":"Author 3","isbn":"ISBN-333","copiesSold":300,"currentEdition":"3rd Edition","issueDate":"2022-01-01"}]};
+    let data: any = {"Books":[{"Title":"Generated Value for Title","Author":"Generated Value for Author","ISBN":"Generated Value for ISBN","Copies Sold":"Generated Value for Copies Sold","Current Edition":"Generated Value for Current Edition","Issue Date":"Generated Value for Issue Date","Genre":"Generated Value for Genre"},{"Title":"Generated Value for Title","Author":"Generated Value for Author","ISBN":"Generated Value for ISBN","Copies Sold":"Generated Value for Copies Sold","Current Edition":"Generated Value for Current Edition","Issue Date":"Generated Value for Issue Date","Genre":"Generated Value for Genre"},{"Title":"Generated Value for Title","Author":"Generated Value for Author","ISBN":"Generated Value for ISBN","Copies Sold":"Generated Value for Copies Sold","Current Edition":"Generated Value for Current Edition","Issue Date":"Generated Value for Issue Date","Genre":"Generated Value for Genre"}]};
     //Autogenerate multiple such calls ie for each resource and its data object
     let isSeeded = await this.IsSeeded();
     //if (!isSeeded) {
